904a734ed53f4bd633f912964dd9e98d11b63b4c
Pedro Emmanuel - Turma 27 - Tribo A <sombraios@hotmail.com>
2023-06-07 18:10:23 -0300

    nova organização de pastas

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+
 import Login from './pages/Login';
 import Character from './pages/Character';
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './frontend/App';
 import { BrowserRouter } from 'react-router-dom';
-import CharacterProvider from './context/CharacterProvider';
+import CharacterProvider from './frontend/context/CharacterProvider';
 
 ReactDOM
   .createRoot(document.getElementById('root'))
